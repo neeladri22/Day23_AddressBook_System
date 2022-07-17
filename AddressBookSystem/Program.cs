2d81af3feb56b3ea71c80113756338a7f8f55a4b
neeladri22 <neeladri22@gmail.com>
2022-07-17 00:22:27 -0700

    No Duplicate entry for same person

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -11,6 +11,33 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook problem");
+
+           
+            Records records = new Records();
+
+
+            //UC7_CheckDuplicate
+            while (true)
+            {
+                Console.WriteLine("\nEnter 1 to Add person in AddressBook");
+                Console.WriteLine("Enter 2 to Display");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        records.AddRecord();
+                        break;
+                    case 2:
+                        records.ToPrint();
+                        break;
+                    default:
+                        Console.WriteLine("Pleasee select valid input");
+                        break;
+
+                }
+
+            }
         }
     }
 }
