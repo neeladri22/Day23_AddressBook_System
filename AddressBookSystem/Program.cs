51db2b9fe6dabf6bb1a104fbc4addebdd4ea75bd
neeladri22 <neeladri22@gmail.com>
2022-07-17 00:31:04 -0700

    Search Person in city or state

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -14,7 +14,7 @@ namespace AddressBookSystem
 
            
             Records records = new Records();
-
+            /*
 
             //UC7_CheckDuplicate
             while (true)
@@ -30,6 +30,39 @@ namespace AddressBookSystem
                         break;
                     case 2:
                         records.ToPrint();
+                        break;
+                    default:
+                        Console.WriteLine("Pleasee select valid input");
+                        break;
+
+                }
+
+            }
+            */
+
+            //UC8 -Search Person in city or State
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter 1 to Add person in AddressBook");
+                Console.WriteLine("Enter 2 to Display");
+                Console.WriteLine("Enter 3 to search the person by city");
+                Console.WriteLine("Enter 4 to search the person by State");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        records.AddRecord();
+                        break;
+                    case 2:
+                        records.ToPrint();
+                        break;
+                    case 3:
+                        records.CheckByCity();
+                        break;
+                    case 4:
+                        records.CheckByState();
                         break;
                     default:
                         Console.WriteLine("Pleasee select valid input");
